84dfa44527049c8fd1140759130b41d59ac40bc8
Sanchy <vsanchez.gassull@gmail.com>
2018-06-01 19:15:50 +0200

    chuleta

--- CLAB_2/chuleta.c
+++ CLAB_2/chuleta.c
@@ -1,13 +1,39 @@
+    /*********************
+         RANDOM STUFF
+    *********************/
+
+    void error(char *msg) {
+        perror(msg);
+        exit(1);
+    }
+
+    void Usage() {
+        char buff[85];
+        sprintf(buff,"USAGE: [insert message]");
+        printf(1,buff,strlen(buff));
+    }
 
     /*********************
         PIPE CON NOBMRE
     *********************/
 
+    //Cualsevol amb permisos hi pot accedir.
+    open(nom,R);
+    open(nom,W);
+    //no es poden obrir a la vegada, sino GG
 
     /*********************
         PIPE SIN NOBMRE
     *********************/
 
+    /*
+    * es crea vector de mida 2:
+    *    · v[0] <- canal entrada (escriptura)
+    *    · v[1] <- canal sortida (lectura)
+    */
+
+    int fd_v[2];
+    int ret = pipe(fd_v);
 
     /*********************
            SOCKETS
@@ -15,10 +41,44 @@
 
 
     /*********************
+            DUPX
+    *********************/
+
+    new_channel = dup(channel);
+    //fa copia del canal que passes per paràmetre
+
+    new_channel = dup2(channel,new_channel);
+    //borra new_channel i duplica channel
+
+    /*********************
             READ
     *********************/
 
+    int ret =  read(int f, void *buffer, sizeof(variable));
+    if (ret == 0) //res a llegir
+    if (ret >  0) //segueix llegint
+    if (ret <  0) error("read");
+
+    /*
+    * ret = num_bytes_llegits;
+    * f es el canal
+    */
 
     /*********************
             WRITE
     *********************/
+
+    int ret =  write(int f, const void *buffer, sizeof(variable));
+    if (ret == 0) //res a llegir
+    if (ret >  0) //segueix llegint
+    if (ret <  0) error("read");
+    ret = num_bytes_escrits;
+
+    /*********************
+            OPEN
+    *********************/
+
+    int file = open("path_to_file", acces_mode, permisos);
+    //file <- nou canal
+    write(file,"string random\n", 14); //escrius al file
+    close(file); //tancar el canal
