456d06c7d8ce9cb0ad250889439f287725daecaf
Sanchy <vsanchez.gassull@gmail.com>
2018-06-04 18:49:46 +0200

    examens anteriors

--- CLAB_2/Examens_anteriors/COMPROC_1/invjump.c
+++ CLAB_2/Examens_anteriors/COMPROC_1/invjump.c
@@ -10,6 +10,11 @@ void Usage() {
 	exit(1);
 }
 
+void error(char *msg) {
+	perror(msg);
+	exit(1);
+}
+
 int main(int argc, char *argv[]) {
 	int i, n, mida, fd_in, fd_out;
 	char c, buff[9];
@@ -21,22 +26,22 @@ int main(int argc, char *argv[]) {
 	for (i=0; i<n; i++) buff[i]='\0';
 
 	fd_out = open("salida", O_WRONLY|O_EXCL|O_CREAT, 0666);
-	if (fd_out<0) panic("open");
+	if (fd_out<0) error("open");
 
 	fd_in = open(argv[1], O_RDONLY);
-	if (fd_in<0) panic("open");
+	if (fd_in<0) error("open");
 
 	mida = lseek(fd_in, 0, SEEK_END);
-	if (mida<0) panic("lseek");
+	if (mida<0) error("lseek");
 
 	for (i=mida-1; i>=0; i--) {
-		if (lseek(fd_in, i, SEEK_SET) <0) panic("lseek");
-		if (read(fd_in, &c, 1) < 0) panic("read");
+		if (lseek(fd_in, i, SEEK_SET) <0) error("lseek");
+		if (read(fd_in, &c, 1) < 0) error("read");
 
-		if (write(fd_out, &c, 1) < 0) panic("write");
+		if (write(fd_out, &c, 1) < 0) error("write");
 
 		if (i>0) {
-			if (write(fd_out, buff, n) < 0) panic("write");
+			if (write(fd_out, buff, n) < 0) error("write");
 		}
 	}
 }
